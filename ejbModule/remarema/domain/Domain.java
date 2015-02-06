16e54d285057adb45ad77bdf6168da44be8759ca
rebeccavl1 <rebeccavanlangelaan@yahoo.com>
2015-02-06 15:03:22 +0100

    init

---
+++ ejbModule/remarema/domain/Domain.java
@@ -0,0 +1,20 @@
+package remarema.domain;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Domain
+ */
+@Stateless
+@LocalBean
+public class Domain {
+
+    /**
+     * Default constructor. 
+     */
+    public Domain() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
